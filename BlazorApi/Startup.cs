be9755bd41a9470cfbf683225ba082946c2d8127
sujanbudathoki <sujanbudathoki24@gmail.com>
2021-01-06 12:13:49 +0545

    /*Added Http Client Service*/
    added http client in startup.cs file

--- BlazorApi/Startup.cs
+++ BlazorApi/Startup.cs
@@ -29,6 +29,8 @@ namespace BlazorApi
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddHttpClient();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
