eeb12fad01eb5f57d06d7afc6fb048c61be9b51b
sujanbudathoki <sujanbudathoki24@gmail.com>
2021-01-08 19:51:10 +0545

    Created Weather Data Model and weather forecast razor

---
+++ BlazorApi/Model/Consolidated_Weather.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BlazorApi.Model
+{
+    public partial class WeatherData
+    {
+        public class Consolidated_Weather
+        {
+            public long id { get; set; }
+            public string weather_state_name { get; set; }
+            public string weather_state_abbr { get; set; }
+            public string wind_direction_compass { get; set; }
+            public DateTime created { get; set; }
+            public string applicable_date { get; set; }
+            public float min_temp { get; set; }
+            public float max_temp { get; set; }
+            public float the_temp { get; set; }
+            public float wind_speed { get; set; }
+            public float wind_direction { get; set; }
+            public float air_pressure { get; set; }
+            public int humidity { get; set; }
+            public float visibility { get; set; }
+            public int predictability { get; set; }
+        }
+
+       
+    }
+}
---
+++ BlazorApi/Model/WeatherData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApi.Model
+{
+    public partial class WeatherData
+    {
+
+
+        public class WeatherDataRoot
+        {
+            public Consolidated_Weather[] consolidated_weather { get; set; }
+            public DateTime time { get; set; }
+            public DateTime sun_rise { get; set; }
+            public DateTime sun_set { get; set; }
+            public string timezone_name { get; set; }
+            
+           
+            public string title { get; set; }
+            public string location_type { get; set; }
+            public int woeid { get; set; }
+            public string latt_long { get; set; }
+            public string timezone { get; set; }
+        }
+
+       
+    }
+}
